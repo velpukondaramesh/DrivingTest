d37f2d38176858377768c54140b2694ffb9c576c
velpukondaramesh@gmail.com <kavitha007>
2017-05-11 18:21:26 +0530

    ad mob issue is resolved

---
+++ app/src/main/java/com/example/ramesh/drivingtest/Test_Activity.java
@@ -0,0 +1,23 @@
+package com.example.ramesh.drivingtest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.ramesh.mysample.R;
+
+/**
+ * Created by Ramesh on 11-05-2017.
+ */
+
+public class Test_Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+
+    }
+}
